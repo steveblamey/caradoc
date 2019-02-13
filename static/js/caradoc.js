252182fd1b5aaaab50f1e709815ee8e1203b7514
Steve Blamey <steve.blamey@adbury.net>
2019-02-13 11:02:58 +0000

    Refactor webmention display, footer, header and post partials

---
+++ static/js/caradoc.js
@@ -0,0 +1,36 @@
+// Show an element
+var show = function (elem) {
+	elem.classList.add('is-visible');
+};
+
+// Hide an element
+var hide = function (elem) {
+	elem.classList.remove('is-visible');
+};
+
+// Toggle element visibility
+var toggle = function (elem) {
+	elem.classList.toggle('is-visible');
+};
+
+// Control webmentions visibility by hash location
+var mentionsVisiblity = function() {
+    var content = document.querySelector('.comments__mentions.toggle-content');
+    var header = document.querySelector('.comments__header.toggle-content');
+    if (!content && !header) return;
+
+    if (location.hash == '#webmentions') {
+        hide(header);
+        show(content);
+    } else {
+        show(header);
+        hide(content);
+    }
+}
+
+mentionsVisiblity();
+
+// Listen for hash location change events
+window.addEventListener('hashchange', function (event) {
+    mentionsVisiblity();
+}, false);
